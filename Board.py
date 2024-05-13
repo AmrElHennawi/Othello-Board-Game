f2eaaad15a469057c7a1a3cba3f28bd292a136f3
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2024-05-13 20:38:59 +0300

    Update Board.py

--- Board.py
+++ Board.py
@@ -38,6 +38,43 @@ class Board:
                 elif self.board[i][j] == -1:
                     self.buttons[i][j].configure(bg="black")
 
+    def UpdateBoard(self, i, j):
+        if self.board[i][j] == 0:
+            neighbors = [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
+            valid_move = any(
+                self.board[x][y] != 0 for x, y in neighbors if 0 <= x < 8 and 0 <= y < 8)
+
+            if not valid_move:
+                messagebox.showinfo(
+                    "Message", "Invalid move! All neighbors are still blank.")
+                self.colorBoard()
+                self.current_player *= 1
+            else:
+                current_color = self.current_player
+                self.board[i][j] = current_color
+                opponent_color = -current_color
+
+                sandwich_made = False
+
+                Sandwich_Moves = self.RepresentMoves(i, j, current_color)
+
+                if not Sandwich_Moves:
+                    messagebox.showinfo(
+                        "Message", "Invalid move! No Sandwich Relationship.")
+                    self.colorBoard()
+                    self.current_player *= 1
+                else:
+                    sandwich_cells = self.find_sandwich_cells(
+                        i, j, current_color)
+
+                    if sandwich_cells:
+                        sandwich_made = True
+                        for cell in sandwich_cells:
+                            self.board[cell[0]][cell[1]] = current_color
+
+                    self.colorBoard()
+                    self.current_player *= -1
+
 
 def main():
     board = Board()
