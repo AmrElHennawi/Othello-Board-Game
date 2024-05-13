428c3d1868a7149ad9772c83c357212534d6ae30
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2024-05-13 21:38:09 +0300

    Update Board.py

--- Board.py
+++ Board.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 
 
 class Board:
@@ -7,73 +8,141 @@ class Board:
         self.window.title("Othello Game")
         self.canvas = tk.Canvas(self.window, width=600,
                                 height=600, background="green")
-        self.window.resizable(width=False, height=False)
+        self.window.resizable(width=True, height=True)
         self.boardSize = 8
         self.board = [[0 for i in range(self.boardSize)]
                       for j in range(self.boardSize)]
+        self.sandwichCells = []
+        self.current_player = -1  # Start with black player
+        self.startBoard()
+        self.createBoard()
+        self.window.mainloop()
+
+    def startBoard(self):
         self.board[3][3] = 1
         self.board[3][4] = -1
         self.board[4][3] = -1
         self.board[4][4] = 1
-        self.create_board()
-        self.window.mainloop()
 
-    def create_board(self):
+    def createBoard(self):
         self.buttons = []
         for i in range(8):
             row = []
             for j in range(8):
                 button = tk.Button(self.window, width=10,
-                                   height=5, background="green")
+                                   height=5, background="green", command=lambda i=i, j=j: self.button_clicked(i, j))
                 button.grid(row=i, column=j)
                 row.append(button)
             self.buttons.append(row)
-        self.color_board()
+        self.RepresentPossibleMoves(self.current_player)
+        self.colorBoard()
 
-    def color_board(self):
+    def colorBoard(self):
         for i in range(8):
             for j in range(8):
                 if self.board[i][j] == 1:
                     self.buttons[i][j].configure(bg="white")
                 elif self.board[i][j] == -1:
                     self.buttons[i][j].configure(bg="black")
+                elif self.board[i][j] == 0:
+                    self.buttons[i][j].configure(bg="green")
+                elif self.board[i][j] == 2:
+                    self.buttons[i][j].configure(bg="red")
+
+    def findSandwichCells(self, current_color):
+        for i in range(self.boardSize):
+            for j in range(self.boardSize):
+                if self.board[i][j] == current_color:
+                    for right in range(j+1, self.boardSize - 1):
+                        if self.board[i][right] == 0:
+                            break
+                        if self.board[i][right] == current_color:
+                            break
+                        if self.board[i][right] == -current_color and self.board[i][right+1] == 0:
+                            self.sandwichCells.append(
+                                (("r"), (i, right+1)))
+
+                    for left in range(j-1, 0, -1):
+                        if self.board[i][left] == 0:
+                            break
+                        if self.board[i][left] == current_color:
+                            break
+                        if self.board[i][left] == -current_color and self.board[i][left-1] == 0:
+                            self.sandwichCells.append(
+                                (("l"), (i, left-1)))
+
+                    for down in range(i+1, self.boardSize - 1):
+                        if self.board[down][j] == 0:
+                            break
+                        if self.board[down][j] == current_color:
+                            break
+                        if self.board[down][j] == -current_color and self.board[down+1][j] == 0:
+                            self.sandwichCells.append(
+                                (("d"), (down+1, j)))
+
+                    for up in range(i-1, 0, -1):
+                        if self.board[up][j] == 0:
+                            break
+                        if self.board[up][j] == current_color:
+                            break
+                        if self.board[up][j] == -current_color and self.board[up-1][j] == 0:
+                            self.sandwichCells.append(
+                                (("u"), (up-1, j)))
+
+    def RepresentPossibleMoves(self, current_color):
+        self.findSandwichCells(current_color)
+        if self.sandwichCells:
+            for cell in self.sandwichCells:
+                self.board[cell[1][0]][cell[1][1]] = 2
+                self.colorBoard()
+            return True
+        return False
+
+    # def colorInBetween(self, i, j, currentColor):
+    #     for cell in self.sandwichCells:
+    #         if cell[0][0] == "r":
+    #             for right in range(j+1, cell[1][1]):
+    #                 if self.board[i][right] == -currentColor:
+    #                     self.board[i][right] = currentColor
+    #         if cell[0][0] == "l":
+    #             for left in range(j-1, cell[1][1], -1):
+    #                 if self.board[i][left] == -currentColor:
+    #                     self.board[i][left] = currentColor
+    #         if cell[0][0] == "d":
+    #             for down in range(i+1, cell[1][0]):
+    #                 if self.board[down][j] == -currentColor:
+    #                     self.board[down][j] = currentColor
+    #         if cell[0][0] == "u":
+    #             for up in range(i-1, cell[1][0], -1):
+    #                 if self.board[up][j] == -currentColor:
+    #                     self.board[up][j] = currentColor
+    #     self.colorBoard()
 
     def UpdateBoard(self, i, j):
-        if self.board[i][j] == 0:
-            neighbors = [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
-            valid_move = any(
-                self.board[x][y] != 0 for x, y in neighbors if 0 <= x < 8 and 0 <= y < 8)
-
-            if not valid_move:
+        currentColor = self.current_player
+        if self.RepresentPossibleMoves(currentColor):
+            if self.board[i][j] == 2:
+                self.board[i][j] = currentColor
+                self.current_player *= -1
+                # self.colorInBetween(i, j, currentColor)
+                self.clearRed()
+            else:
                 messagebox.showinfo(
-                    "Message", "Invalid move! All neighbors are still blank.")
+                    "Message", "Invalid move! No Sandwich Relationship.")
                 self.colorBoard()
                 self.current_player *= 1
-            else:
-                current_color = self.current_player
-                self.board[i][j] = current_color
-                opponent_color = -current_color
 
-                sandwich_made = False
+    def clearRed(self):
+        self.sandwichCells.clear()
+        for i in range(8):
+            for j in range(8):
+                if self.board[i][j] == 2:
+                    self.board[i][j] = 0
+        self.colorBoard()
 
-                Sandwich_Moves = self.RepresentMoves(i, j, current_color)
-
-                if not Sandwich_Moves:
-                    messagebox.showinfo(
-                        "Message", "Invalid move! No Sandwich Relationship.")
-                    self.colorBoard()
-                    self.current_player *= 1
-                else:
-                    sandwich_cells = self.find_sandwich_cells(
-                        i, j, current_color)
-
-                    if sandwich_cells:
-                        sandwich_made = True
-                        for cell in sandwich_cells:
-                            self.board[cell[0]][cell[1]] = current_color
-
-                    self.colorBoard()
-                    self.current_player *= -1
+    def button_clicked(self, row, col):
+        self.UpdateBoard(row, col)
+        self.RepresentPossibleMoves(self.current_player)
 
 
 def main():
